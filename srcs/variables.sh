b2be783d1a000cade941e8ebc2060976ce10d44d
ska42 <skalyxe00@gmail.com>
2021-08-11 09:30:32 +0200

    move striteri

--- srcs/variables.sh
+++ srcs/variables.sh
@@ -46,15 +46,15 @@ Part1_func=('ft_memset.c' 'ft_bzero.c' 'ft_calloc.c' 'ft_memcpy.c' 'ft_memccpy.c
 #                 Part2 functions                 #
 ###################################################
 
-Part2_func_authorized=('1' '1' '1' '1' '1' '3' \
+Part2_func_authorized=('1' '1' '1' '1' '3' \
 '1' \
 '4' '4' '4' '4')
 
-Part2_func_activation=('0' '0' '0' \
+Part2_func_activation=('0' '0' \
 '0' '0' '0' '0' '0' \
 '0' '0' '0')
 
-Part2_func=('ft_strmapi.c' 'ft_striteri.c' 'ft_substr.c' 'ft_strjoin.c' 'ft_strtrim.c' 'ft_split.c' \
+Part2_func=('ft_strmapi.c' 'ft_substr.c' 'ft_strjoin.c' 'ft_strtrim.c' 'ft_split.c' \
 'ft_itoa.c' \
 'ft_putchar_fd.c' 'ft_putstr_fd.c' 'ft_putendl_fd.c' 'ft_putnbr_fd.c')
 
@@ -75,19 +75,19 @@ Bonus_func_authorized=('3' '2' '2' '0' '0' '0' '0' '3' '0')
 Additional_func=('ft_isblank.c' 'ft_iscntrl.c' 'ft_isgraph.c' 'ft_islower.c' \
 'ft_isspace.c' 'ft_isupper.c' 'ft_isxdigit.c' 'ft_str_is_alpha.c' 'ft_str_is_lowercase.c' \
 'ft_str_is_numeric.c' 'ft_str_is_printable.c' 'ft_str_is_uppercase.c' 'ft_strcapitalize.c' \
-'ft_strcasecmp.c' 'ft_strlowcase.c' 'ft_strndup.c' 'ft_strupcase.c' \
+'ft_strcasecmp.c' 'ft_strlowcase.c' 'ft_striteri.c' 'ft_strndup.c' 'ft_strupcase.c' \
 'ft_itoa_base.c' 'ft_strncasecmp.c' 'ft_strcasestr.c' 'ft_strtok.c')
 
 Additional_func_activation=('0' '0' '0' '0' \
 '0' '0' '0' '0' '0' \
 '0' '0' '0' '0' \
-'0' '0' '0' '0' \
+'0' '0' '0' '0' '0' \
 '0' '0' '0' '0')
 
 Additional_func_authorized=('0' '0' '0' '0' \
 '0' '0' '0' '0' '0' \
 '0' '0' '0' '0' \
-'0' '0' '0' '1' \
+'0' '0' '0' '0' '1' '0' \
 '1' '0' '0' '0')
 
 ###################################################
