a9db2965a6d3368b8080517ac7ce2d083fa90f72
Fedor Dikarev <fdikarev@student.42berlin.de>
2024-05-15 21:46:45 +0200

    Revert "Remove `-fsanitize=address`"
    This reverts commit a8235351dc16645a6127a69c3cd4f2d632aab3c6.

--- srcs/check_compilation.sh
+++ srcs/check_compilation.sh
@@ -18,8 +18,8 @@ compilation()
 	then
 		rm -f "${PATH_TEST}"/user_exe
 	fi
-	printf "$> clang -Wextra -Wall -Werror -g3 $1 main.c libft.a -o user_exe\n\n" >> "${PATH_DEEPTHOUGHT}"/deepthought
-	clang -Wextra -Wall -Werror -g3 "${PATH_TEST}"/dirlibft/${SRC_DIR}/$1 \
+	printf "$> clang -Wextra -Wall -Werror -g3 -fsanitize=address $1 main.c libft.a -o user_exe\n\n" >> "${PATH_DEEPTHOUGHT}"/deepthought
+	clang -Wextra -Wall -Werror -g3 -fsanitize=address "${PATH_TEST}"/dirlibft/${SRC_DIR}/$1 \
 		  "${PATH_TEST}"/tests/$(echo ${part}tions)/$(echo $1 | cut -d . -f 1 | sed 's/_bonus//g')/main.c \
 		  -I "${PATH_LIBFT}"/${HEADER_DIR}/ \
 		  "${PATH_TEST}"/dirlibft/libft.a 2>>"${PATH_DEEPTHOUGHT}"/deepthought -o user_exe
